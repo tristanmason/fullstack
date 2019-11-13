4bd97ed1527cfce8a4de3edd2b66d1e2b0657151
Tristan Mason <tristan@codeshimmer.com>
2019-11-12 22:48:51 -0600

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -20,18 +20,54 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [allFeedback, setAll] = useState([])
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+        setAll(allFeedback.concat(1))
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+        setAll(allFeedback.concat(0))
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+        setAll(allFeedback.concat(-1))
+    }
+
+    const average = (array) => {
+        if (array.length !== 0) {
+            let total = array.reduce((sum, num) => sum + num)
+            return total / array.length
+        } else {
+            return "-"
+        }
+    }
+
+    const percentPositive = (positive, all) => {
+        if (positive) {
+            return ( (positive / all) * 100 ) + "%"
+        } else {
+            return "-"
+        }
+    }
 
     return (
         <div class="wrapper">
             <Header title="Provide Feedback" />
-            <Button onClick={() => setGood(good + 1)} text="Good" />
-            <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
-            <Button onClick={() => setBad(bad + 1)} text="Bad" />
+            <Button onClick={handleGoodClick} text="Good" />
+            <Button onClick={handleNeutralClick} text="Neutral" />
+            <Button onClick={handleBadClick} text="Bad" />
 
             <Header title="Statistics" />
             <Stats text="Good" stat={good} />
             <Stats text="Neutral" stat={neutral} />
             <Stats text="Bad" stat={bad} />
+            <Stats text="All" stat={allFeedback.length} />
+            <Stats text="Average" stat={average(allFeedback)} />
+            <Stats text="Positive" stat={percentPositive(good, allFeedback.length)} />
         </div>
     )
 }
