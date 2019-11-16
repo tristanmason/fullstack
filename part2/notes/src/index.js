d6bf8ce29964f557037f8b21b4c3cf83e3b76aa8
Tristan Mason <tristan@codeshimmer.com>
2019-11-15 22:28:56 -0600

    notes example

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
 const notes = [
     {
@@ -22,21 +23,6 @@ const notes = [
         important: true
     }
 ]
-
-const App = (props) => {
-    const { notes } = props
-
-    return (
-        <div>
-            <h1>Notes</h1>
-            <ul>
-                <li>{notes[0].content}</li>
-                <li>{notes[1].content}</li>
-                <li>{notes[2].content}</li>
-            </ul>
-        </div>
-    )
-}
 
 ReactDOM.render(
     <App notes={notes} />,
