ef70019c41eecdede3b216d5181d31fc0b550666
Tristan Mason <tristan@codeshimmer.com>
2019-11-17 22:49:03 -0600

    2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,67 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
+    { name: 'Ada Lovelace' }
   ])
   const [newName, setNewName] = useState('')
 
+  const Person = ({ person }) => {
+    return (
+      <tr>
+        <td>{person.name}</td>
+      </tr>
+    )
+  }
+
+  const rows = () => persons.map(person =>
+    <Person
+      key={person.name}
+      person={person}
+    />
+  )
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
+    <div className="wrapper">
+      <h1>Phonebook</h1>
+      <form onSubmit={addName}>
+        <div className="form-group">
+          <label htmlFor="addNameInput">
+            Name
+          </label>
+          <input
+            name="addNameInput"
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <table>
+        <tbody>
+          {rows()}
+        </tbody>
+      </table>
+      
+      <p>debug: {newName}</p>
     </div>
   )
 }
