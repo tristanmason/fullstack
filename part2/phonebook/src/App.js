38cd08db06ee46710158f97d447bbede277ef905
Tristan Mason <tristan@codeshimmer.com>
2019-11-24 14:48:19 -0600

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,17 @@ import React, { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '040-1234567' },
-    { name: 'Ada Lovelace', phone: '044-7654321' }
+    { name: 'Ada Lovelace', phone: '044-7654321' },
+    { name: 'Codey McCodeface', phone: '123-456-7890'},
+    { name: 'Foo Foobarsson', phone: '441-555-8967' },
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [searchTerms, setSearchTerms] = useState('')
+
+  const filteredPersons = searchTerms
+    ? persons.filter(person => (new RegExp(searchTerms, 'i').test(person.name)))
+    : persons
 
   const Person = ({ person }) => {
     return (
@@ -16,7 +23,7 @@ const App = () => {
     )
   }
 
-  const rows = () => persons.map(person =>
+  const rows = () => filteredPersons.map(person =>
     <Person
       key={person.name}
       person={person}
@@ -49,9 +56,29 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearchTerms(event.target.value)
+  }
+
   return (
     <div className="wrapper">
       <h1>Phonebook</h1>
+      <form>
+        <div className="form-group">
+          <p>
+            <label htmlFor="addNameInput">
+              Filter
+            </label>
+            <input
+              name="searchInput"
+              value={searchTerms}
+              placeholder="Start typing a name"
+              onChange={handleSearchChange}
+            />
+          </p>
+        </div>
+      </form>
+      <h2>Add New</h2>
       <form onSubmit={addName}>
         <div className="form-group">
           <p>
