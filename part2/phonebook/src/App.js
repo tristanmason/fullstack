3a9a5cc5c21cc7139a7cb9e62d53db5411da78bd
Tristan Mason <tristan@codeshimmer.com>
2019-11-24 15:52:36 -0600

    2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,83 @@
 import React, { useState } from 'react'
 
+const Filter = ({ searchTerms, handleSearchChange }) => {
+  return (
+    <form>
+      <div className="form-group">
+        <p>
+          <label htmlFor="addNameInput">
+            Filter
+              </label>
+          <input
+            name="searchInput"
+            value={searchTerms}
+            placeholder="Start typing a name"
+            onChange={handleSearchChange}
+          />
+        </p>
+      </div>
+    </form>
+  )
+}
+
+const PersonForm = ({ addName, newName, newPhone, handleNameChange, handlePhoneChange }) => {
+
+  return (
+    <form onSubmit={addName}>
+      <div className="form-group">
+        <p>
+          <label htmlFor="addNameInput">
+            Name
+              </label>
+          <input
+            name="addNameInput"
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </p>
+        <p>
+          <label htmlFor="addPhoneInput">
+            Phone
+              </label>
+          <input
+            name="addPhoneInput"
+            value={newPhone}
+            onChange={handlePhoneChange}
+          />
+        </p>
+      </div>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({filteredPersons}) => {
+  const rows = () => filteredPersons.map(person =>
+    <Person
+      key={person.name}
+      person={person}
+    />
+  )
+
+  return (
+    <table>
+      <tbody>
+        {rows()}
+      </tbody>
+    </table>
+  )
+}
+
+const Person = ({ person }) => {
+  return (
+    <tr>
+      <td>{person.name} {person.phone}</td>
+    </tr>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '040-1234567' },
@@ -15,21 +93,6 @@ const App = () => {
     ? persons.filter(person => (new RegExp(searchTerms, 'i').test(person.name)))
     : persons
 
-  const Person = ({ person }) => {
-    return (
-      <tr>
-        <td>{person.name} {person.phone}</td>
-      </tr>
-    )
-  }
-
-  const rows = () => filteredPersons.map(person =>
-    <Person
-      key={person.name}
-      person={person}
-    />
-  )
-
   const addName = (event) => {
     event.preventDefault()
 
@@ -38,8 +101,7 @@ const App = () => {
       phone: newPhone
     }
 
-    if (persons.find(person => (person.name === personObject.name)))
-    {
+    if (persons.find(person => (person.name === personObject.name))) {
       alert(`${newName} has already been added to your phonebook`)
     } else {
       setPersons(persons.concat(personObject))
@@ -63,55 +125,22 @@ const App = () => {
   return (
     <div className="wrapper">
       <h1>Phonebook</h1>
-      <form>
-        <div className="form-group">
-          <p>
-            <label htmlFor="addNameInput">
-              Filter
-            </label>
-            <input
-              name="searchInput"
-              value={searchTerms}
-              placeholder="Start typing a name"
-              onChange={handleSearchChange}
-            />
-          </p>
-        </div>
-      </form>
+
+      <Filter searchTerms={searchTerms} handleSearchChange={handleSearchChange} />
+      
       <h2>Add New</h2>
-      <form onSubmit={addName}>
-        <div className="form-group">
-          <p>
-            <label htmlFor="addNameInput">
-              Name
-            </label>
-            <input
-              name="addNameInput"
-              value={newName}
-              onChange={handleNameChange}
-            />
-          </p>
-          <p>
-            <label htmlFor="addPhoneInput">
-              Phone
-            </label>
-            <input
-              name="addPhoneInput"
-              value={newPhone}
-              onChange={handlePhoneChange}
-            />
-          </p>
-        </div>
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form>
+
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        newPhone={newPhone}
+        handleNameChange={handleNameChange}
+        handlePhoneChange={handlePhoneChange}
+      />
+
       <h2>Numbers</h2>
-      <table>
-        <tbody>
-          {rows()}
-        </tbody>
-      </table>
+
+      <Persons filteredPersons={filteredPersons} />
     </div>
   )
 }
