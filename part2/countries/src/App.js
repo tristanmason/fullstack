8113527eb198bc1854d4c798b5b486b882b0b00b
Tristan Mason <tristan@codeshimmer.com>
2019-11-26 10:26:58 -0600

    2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -7,7 +7,7 @@ const Filter = ({ searchTerms, handleSearchChange }) => {
       <div className="form-group">
         <p>
           <label htmlFor="addNameInput">
-            Filter
+            Find Countries
               </label>
           <input
             name="searchInput"
@@ -35,6 +35,20 @@ const Button = ({ onClick, text, country }) => (
   </button>
 )
 
+const Weather = ({ city, weather }) => {
+
+  return (
+    <div className="countryWeather">
+      <h2>Weather in {city}</h2>
+      <img src={weather.weather_icons} alt="weather icon" />
+      <p>{weather.weather_descriptions}</p>
+      <p><b>temperature: </b>{weather.temperature} Celsius</p>
+      <p><b>wind: </b>{weather.wind_speed} kph direction {weather.wind_dir}</p>
+    </div>
+  )
+
+}
+
 const Info = ({ countryInfo }) => {
   const languages = () => {
     return (
@@ -47,7 +61,7 @@ const Info = ({ countryInfo }) => {
   }
   
   return (
-    <div>
+    <div className="countryInfo">
       <h2>{countryInfo.name}</h2>
       <p>
         Capital: {countryInfo.capital}<br />
@@ -66,7 +80,7 @@ const Info = ({ countryInfo }) => {
   )
 }
 
-const Countries = ({ filteredCountries, handleShowClick }) => {
+const Countries = ({ filteredCountries, handleShowClick, handleCountrySelect, weather }) => {
 
   const rows = () => {
     if (filteredCountries.length === 0) {
@@ -74,9 +88,13 @@ const Countries = ({ filteredCountries, handleShowClick }) => {
       return "No results"
     
     } else if (filteredCountries.length === 1) {
+      handleCountrySelect(filteredCountries[0])
     
       return (
-        <Info countryInfo={filteredCountries[0]} />
+        <div className="infoWrapper">
+          <Info countryInfo={filteredCountries[0]} />
+          <Weather city={filteredCountries[0].capital} weather={weather} />
+        </div>
       )
     
     } else if (filteredCountries.length > 10) {
@@ -84,7 +102,7 @@ const Countries = ({ filteredCountries, handleShowClick }) => {
       return "Too many matches, please be more specific"
     
     } else if (filteredCountries.length > 1) {
-    
+
       return (
         filteredCountries.map(country =>
           <div className="result" key={country.name}>
@@ -111,7 +129,9 @@ const Countries = ({ filteredCountries, handleShowClick }) => {
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState([])
   const [searchTerms, setSearchTerms] = useState('')
+  const [weather, setWeather] = useState([])
 
   useEffect(() => {
     axios
@@ -120,6 +140,23 @@ const App = () => {
         setCountries(response.data)
       })
   }, [])
+
+  useEffect(() => {
+    if (selectedCountry.length !== 0) {
+      console.log(selectedCountry)
+      const city = selectedCountry.capital
+      const baseUrl = 'http://api.weatherstack.com/current?access_key='
+      const accessKey = '40127fb36d15b56f0fb54b4d83e3624c'
+      const cityEncoded = encodeURIComponent(city.replace(/[.,]/g, ""))
+
+      axios
+        .get(`${baseUrl}${accessKey}&query=${cityEncoded}`)
+        .then(response => {
+          setWeather(response.data.current)
+          console.log(response.data.current)
+        })
+      }
+  }, [selectedCountry])
 
   const filteredCountries = searchTerms
     ? countries.filter(country => (new RegExp(searchTerms, 'i').test(country.name)))
@@ -133,14 +170,15 @@ const App = () => {
     setSearchTerms(event.target.getAttribute('data-country'))
   }
 
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country)
+  }
+
   return (
     <div className="wrapper">
       <h1>Country Info</h1>
-
       <Filter searchTerms={searchTerms} handleSearchChange={handleSearchChange} />
-
-      <Countries filteredCountries={filteredCountries} handleShowClick={handleShowClick} />
-
+      <Countries filteredCountries={filteredCountries} handleShowClick={handleShowClick} handleCountrySelect={handleCountrySelect} weather={weather} />
     </div>
   )
 }
